d541b3f4d4b7bc99a0199618f993092918450fca
Matthew Boeh <matthew.boeh@gmail.com>
2013-02-07 13:22:24 -0800

    Don't try to set dispatcher_type to :bunny in production environments.

--- lib/woodhouse/rails.rb
+++ lib/woodhouse/rails.rb
@@ -34,11 +34,6 @@ if defined?(Rails::Railtie)
       # Set up reasonable defaults
       Woodhouse.configure do |config|
         config.logger = ::Rails.logger
-        if ::Rails.env =~ /^(development|test)$/
-          config.dispatcher_type = :local
-        else
-          config.dispatcher_type = :bunny
-        end
         config_paths.each do |path|
           if File.exist?(path)
             config.server_info = YAML.load(File.read(path))[::Rails.env]
