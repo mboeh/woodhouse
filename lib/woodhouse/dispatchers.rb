ed7e6e7af964cbefb8e4a3a37b32d3a170a75ac6
Matthew Boeh <matthew.boeh@gmail.com>
2013-03-04 22:30:27 -0800

    Provide an :amqp dispatcher type which automatically chooses hot_bunnies/bunny based on the Ruby engine.

--- lib/woodhouse/dispatchers.rb
+++ lib/woodhouse/dispatchers.rb
@@ -1,5 +1,13 @@
 module Woodhouse::Dispatchers
 
+  def self.default_amqp_dispatcher
+    if RUBY_ENGINE =~ /jruby/
+      Woodhouse::Dispatchers::HotBunniesDispatcher
+    else
+      Woodhouse::Dispatchers::BunnyDispatcher
+    end
+  end
+
 end
 
 require 'woodhouse/dispatcher'
@@ -7,3 +15,5 @@ require 'woodhouse/dispatchers/local_dispatcher'
 require 'woodhouse/dispatchers/bunny_dispatcher'
 require 'woodhouse/dispatchers/hot_bunnies_dispatcher'
 require 'woodhouse/dispatchers/local_pool_dispatcher'
+
+Woodhouse::Dispatchers::AmqpDispatcher = Woodhouse::Dispatchers.default_amqp_dispatcher 
